acb9c838f9c6fad22a069ad72140189919fe5e86
Muti <mutiarapandejlaki47@gmail.com>
2026-04-11 15:36:30 +0800

    tambah icon

--- src/shared/components/UserNavigation.jsx
+++ src/shared/components/UserNavigation.jsx
@@ -4,10 +4,10 @@ function Navigation() {
   return (
     <nav>
       <ul className="navigation">
-        <li className="nav-links"><a href="/">Beranda</a></li>
-        <li className="nav-links"><a href="/profil-sejarah">Profil & Sejarah</a></li>
-        <li className="nav-links"><a href="/articles">Artikel</a></li>
-        <li className="nav-links"><a href="/contact">Kontak</a></li>
+        <li className="nav-links"><a href="/"><i className="fa-solid fa-house"></i> Beranda</a></li>
+        <li className="nav-links"><a href="/profil-sejarah"><i className="fa-solid fa-info-circle"></i> Profil & Sejarah</a></li>
+        <li className="nav-links"><a href="/articles"><i className="fa-solid fa-newspaper"></i> Artikel</a></li>
+        <li className="nav-links"><a href="/contact"><i className="fa-solid fa-phone"></i> Kontak</a></li>
       </ul>
     </nav>
   );

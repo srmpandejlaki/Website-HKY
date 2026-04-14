c1fd68746bd9e88c2eff81e4793da2231b80593d
Muti <mutiarapandejlaki47@gmail.com>
2026-04-14 23:58:09 +0800

    tambah halaman kontak

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import MainLayout from "./shared/layouts/MainLayout/MainLayout";
 import DashboardUmat from "./features/user-dashboard/pages/Dashboard";
 import ProfilSejarah from "./features/user-sejarah-visimisi/pages/ProfilSejarah";
 import Berita from "./features/user-berita/pages/Berita";
+import Contact from "./features/user-contact/pages/Contact";
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
           <Route path="dashboard" element={<DashboardUmat />} />
           <Route path="profil-sejarah" element={<ProfilSejarah />} />
           <Route path="berita" element={<Berita />} />
+          <Route path="contact" element={<Contact />} />
         </Route>
       </Routes>
     </main>
---
+++ src/features/user-contact/pages/Contact.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <div className="contact-section">
+      <h2>Contact Us</h2>
+      <p>If you have any questions or inquiries, please feel free to contact us:</p>
+      <ul>
+        <li>Email: info_hky.tomohon@go.id</li>
+        <li>Phone: (0431) 123-456</li>
+        <li>Address: Jl. Raya Tomohon, Kolongan, Kec. Tomohon Tengah, Kota Tomohon, Sulawesi Utara</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Contact;

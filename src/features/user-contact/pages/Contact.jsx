3e5bdfda9bc75a33f5f622c386926016fb1c1546
Muti <mutiarapandejlaki47@gmail.com>
2026-04-15 23:46:23 +0800

    ubah layout page contact

--- src/features/user-contact/pages/Contact.jsx
+++ src/features/user-contact/pages/Contact.jsx
@@ -5,21 +5,6 @@ function Contact() {
     <div className="contact-section">
       <h2 className="contact-title">Contact Us</h2>
       <div className="contact-main">
-        <div className="map-container">
-          <a 
-            href="https://maps.app.goo.gl/R1XL7atKxPSA8PTw6"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.7573815870373!2d124.83552347394506!3d1.3213438616689444!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x32876c97b0aabd3d%3A0xdcbb9ba076f221d2!2sGereja%20Katolik%20Paroki%20Hati%20Kudus%20Yesus%2C%20Tomohon%20-%20Kolongan!5e0!3m2!1sid!2sid!4v1776263565686!5m2!1sid!2sid"
-              width="100%"
-              height="250"
-              style={{ border: 0 }}
-              loading="lazy"
-            ></iframe>
-          </a>
-        </div>
         <div className="contact-number">
           <p>HUBUNGI KAMI MELALUI</p>
           <ul>
@@ -35,6 +20,22 @@ function Contact() {
             <li><a href="https://www.instagram.com/parokihatiKudusYesus" target="_blank" rel="noopener noreferrer">Instagram</a></li>
           </ul>
         </div>
+      </div>
+      <div className="map-container">
+        <h3 className="map-title">Lokasi Gereja Hati Kudus Yesus Tomohon</h3>
+        <a 
+          href="https://maps.app.goo.gl/R1XL7atKxPSA8PTw6"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.7573815870373!2d124.83552347394506!3d1.3213438616689444!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x32876c97b0aabd3d%3A0xdcbb9ba076f221d2!2sGereja%20Katolik%20Paroki%20Hati%20Kudus%20Yesus%2C%20Tomohon%20-%20Kolongan!5e0!3m2!1sid!2sid!4v1776263565686!5m2!1sid!2sid"
+            width="100%"
+            height="250"
+            style={{ border: 0 }}
+            loading="lazy"
+          ></iframe>
+        </a>
       </div>
     </div>
   );
